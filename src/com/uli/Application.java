34c06969ff2ef3b6493e1109a057c4875378fdef
Yulian <57912008+uli-uli@users.noreply.github.com>
2020-12-25 01:25:18 +0200

    Updating color argument of Rectangular program

--- src/com/uli/Application.java
+++ src/com/uli/Application.java
@@ -1,10 +1,13 @@
 package com.uli;
 
+import java.util.Random;
+
 public class Application {
     public static void main(String[] args) {
         Rectangular rectangularFirst = new Rectangular();
-        Rectangular rectangularSecond = new Rectangular(10, 5, null, null);
+        Rectangular rectangularSecond = new Rectangular(10, 5, "SuperRectangular", Rectangular.randomColor[new Random().nextInt(Rectangular.randomColor.length)]);
         Rectangular rectangularThird = new Rectangular(rectangularSecond);
+        rectangularSecond.setLength(11);
         System.out.println(rectangularFirst);
         System.out.println(rectangularSecond);
         System.out.println(rectangularThird);
